e4053809d4313e0e1e7ae096e84aaa01d6f610d2
Lars Thoennes <larsthoennes@gmail.com>
2026-04-08 20:08:33 +0000

    update requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-duc5tgdue&lvfshn=*-n%6zh@pb^k$b3uw$xp82hw8ev%3%#8i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["lars-thoennes.developerakademie.org","lars-thoennes.de","127.0.0.1","0.0.0.0","localhost"]
 
 
 # Application definition
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -34,4 +33,4 @@ urlpatterns = [
     path('api/', include('reviews_app.api.urls')),
 
     path('api/', include('platform_app.api.urls')),
-]
+] + staticfiles_urlpatterns()
